f23ec7541ea16285ec6844cc782fe0870d4d45cf
yoshiiron <eynrus@gmail.com>
2025-07-18 01:15:43 +0300

    add config file

---
+++ frontend/src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    backendContainerName: "backend",
+    backendPort: "8080",
+};
+
+console.log(config.backendContainerName);
+console.log(config.backendPort);
+
+export default config;
